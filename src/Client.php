bd434d321bb2a63795dab53165106bde73ed1b75
lichunqiang <light-li@hotmail.com>
2014-12-23 22:18:24 +0800

    initialize

---
+++ src/Client.php
@@ -0,0 +1,10 @@
+<?php
+namespace ecdarwin;
+
+class Client
+{
+	public function version()
+	{
+		return '0.0.0';
+	}
+}
---
+++ src/adapters/StringAdapter.php
@@ -0,0 +1,7 @@
+<?php
+namespace ecdarwin\adapters;
+
+class StringAdapter
+{
+	
+}
